c075c0cc29243e984914a2df8b7aa47417393162
Francisco Parra <franparpe@disroot.org>
2023-05-15 14:05:33 +0200

    Refactoring files

--- safein/src/app/app-routing.module.ts
+++ safein/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BookingsViewerColumnBookingsComponent } from './bookings-viewer-column-bookings/bookings-viewer-column-bookings.component';
-import { BookingsViewerColumnCitiesComponent } from './bookings-viewer-column-cities/bookings-viewer-column-cities.component';
-import { BookingsViewerColumnCountriesComponent } from './bookings-viewer-column-countries/bookings-viewer-column-countries.component';
-import { BookingsViewerColumnHotelsComponent } from './bookings-viewer-column-hotels/bookings-viewer-column-hotels.component';
-import { BookingsViewerColumnComponent } from './bookings-viewer-column/bookings-viewer-column.component';
-import { HomeLatestBookingsComponent } from './home-latest-bookings/home-latest-bookings.component'
+import { BookingsViewerColumnBookingsComponent } from './components/bookings-viewer-column-bookings/bookings-viewer-column-bookings.component';
+import { BookingsViewerColumnCitiesComponent } from './components/bookings-viewer-column-cities/bookings-viewer-column-cities.component';
+import { BookingsViewerColumnCountriesComponent } from './components/bookings-viewer-column-countries/bookings-viewer-column-countries.component';
+import { BookingsViewerColumnHotelsComponent } from './components/bookings-viewer-column-hotels/bookings-viewer-column-hotels.component';
+import { BookingsViewerColumnComponent } from './components/bookings-viewer-column/bookings-viewer-column.component';
+import { HomeLatestBookingsComponent } from './components/home-latest-bookings/home-latest-bookings.component'
 
 const routes: Routes = [
 	{ // Home
