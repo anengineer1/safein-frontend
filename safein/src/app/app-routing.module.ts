fdde81120fdcead380c767861ccec0be705dca86
Francisco Parra <franparpe@disroot.org>
2023-05-18 18:45:23 +0200

    Rearrenging components

--- safein/src/app/app-routing.module.ts
+++ safein/src/app/app-routing.module.ts
@@ -8,40 +8,47 @@ import { BookingsViewerColumnComponent } from './components/bookings/bookings-vi
 import { HomeLatestBookingsComponent } from './components/home-latest-bookings/home-latest-bookings.component'
 import { BookingsViewerColumnRoomsComponent } from './components/bookings/bookings-viewer-column-rooms/bookings-viewer-column-rooms.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BookingsComponent } from './pages/bookings/bookings.component';
 
 const routes: Routes = [
+	{ path: '', redirectTo: '/home', pathMatch: 'full' },
 	{ // Home
 		path: "home",
 		component: HomeComponent, children: [
 			{
-				path:"",
+				path: "latestbookings",
 				component: HomeLatestBookingsComponent
 			}
 		]
 	},
 	{ // Bookings route
-		path: "bookings", component: BookingsViewerColumnComponent, children: [
-			{
-				path: "bookingsview",
-				component: BookingsViewerColumnBookingsComponent
-			},
-			{
-				path: "countriesview",
-				component: BookingsViewerColumnCountriesComponent
-			},
-			{
-				path: "citiesview",
-				component: BookingsViewerColumnCitiesComponent
-			},
-			{
-				path: "hotelsview",
-				component: BookingsViewerColumnHotelsComponent
-			},
-			{
-				path: "roomsview",
-				component: BookingsViewerColumnRoomsComponent
-			}
+		path: "bookings",
+		component: BookingsComponent, children: [
+			{ // Bookings route
+				path: "bookings", component: BookingsViewerColumnComponent, children: [
+					{
+						path: "bookingsview",
+						component: BookingsViewerColumnBookingsComponent
+					},
+					{
+						path: "countriesview",
+						component: BookingsViewerColumnCountriesComponent
+					},
+					{
+						path: "citiesview",
+						component: BookingsViewerColumnCitiesComponent
+					},
+					{
+						path: "hotelsview",
+						component: BookingsViewerColumnHotelsComponent
+					},
+					{
+						path: "roomsview",
+						component: BookingsViewerColumnRoomsComponent
+					}
 
+				]
+			}
 		]
 	}
 ];
