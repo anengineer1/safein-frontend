f1d133a446fac94450beb995a3e3dffdc8222d06
eilmadc@gmail.com <eilmadc@gmail.com>
2023-05-21 11:34:15 +0200

    Update signin.ts with logic code jwt

--- safein/src/app/components/user-account/signin/signin.component.ts
+++ safein/src/app/components/user-account/signin/signin.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/_services/auth.service';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit{
+
+  
+  form: any ={
+    username: null,
+    password: null
+  };
+
+  isLoggedIn: boolean = false;
+  isLoggedFailed: boolean = false;
+  errorMessage: string = '';
+  roles: String[] = [];
+
+  /* Constructor with Auth and TokenStorage services as params */
+  constructor(private authService: AuthService, private ts: TokenStorageService){  }
+  
+  
+  
+  /* Oninit */
+  ngOnInit(): void {
+
+    //if get the token then change state of isLoggegIn and roles
+    if(this.ts.getToken()){
+      this.isLoggedIn = true;
+      this.roles = this.ts.getUser().roles;
+    }
+  }
+
+  /* OnSubmit Form */
+  onSumbit(): void {
+    const {username, password} = this.form;
+
+    this.authService.login(username, password).subscribe(data => {
+      this.ts.saveToken(data.accessToken);
+      this.ts.saveUser(data);
+
+      this.isLoggedFailed = false;
+      this.isLoggedIn = true;
+      this.roles = this.ts.getUser().roles;
+      this.reloadPage();
+    },
+    err =>{
+      this.errorMessage = err.error.message;
+      this.isLoggedFailed = true;
+    });
+  }
+  
+  reloadPage() {
+    throw new Error('Method not implemented.');
+  }
 
 }
+
