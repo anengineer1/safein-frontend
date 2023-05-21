aa56c451e39ca7f141bf46cb65639bd15656b865
eilmadc@gmail.com <eilmadc@gmail.com>
2023-05-21 10:27:27 +0200

    added Interceptor

---
+++ safein/src/app/_helpers/auth.interceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TokenStorageService } from '../_services/token-storage.service';
+import { KEYS } from '../enviroments/KEYS';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  
+  constructor(private token: TokenStorageService) { }
+
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let authReq = req;
+    const token = this.token.getToken();
+
+    if (token != null) {
+      authReq = req.clone({
+        headers: req.headers.set(KEYS.TOKEN_HEADER_KEY, 'Bearer ' + token)
+      });
+    }
+    
+    return next.handle(authReq);
+  }
+}
+
+export const authInterceptorProvider = [
+  { provide: HTTP_INTERCEPTORS, userClass: AuthInterceptor, multi: true }
+]
