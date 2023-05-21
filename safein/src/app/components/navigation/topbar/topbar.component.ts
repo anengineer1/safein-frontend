c0e93d8c561f4b273eeb93f6add7b326ad3bc80e
eilmadc@gmail.com <eilmadc@gmail.com>
2023-05-21 11:14:22 +0200

    Update topbar settings to show User and Rol and Logout. Additionally fix hover color dropdown menu user

--- safein/src/app/components/navigation/topbar/topbar.component.ts
+++ safein/src/app/components/navigation/topbar/topbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
 
 @Component({
   selector: 'app-topbar',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./topbar.component.scss']
 })
 export class TopbarComponent {
+  
+  //JWT
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+  role?: string;
+
+  //Constructor with TokenStorageService as a parameter
+  constructor(private tss: TokenStorageService) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tss.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tss.getUser();
+      this.roles = user.roles;
+      this.role = user.role;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.tss.signOut();
+    window.location.reload();    
+  }
 
 }
