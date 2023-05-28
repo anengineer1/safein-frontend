9bce0d880af06b8344e0acb48679b8203fda3b6f
eilmadc@gmail.com <eilmadc@gmail.com>
2023-05-28 11:51:26 +0200

    disable scrolls from styles + fix little error

--- safein/src/app/services/customers.service.ts
+++ safein/src/app/services/customers.service.ts
@@ -36,17 +36,6 @@ export class CustomersService {
       .pipe(catchError(this.handleError));
   }
 
-  handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      console.error("An error occurred:", error.error.message);
-    } else {
-      console.error(
-        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
-      );
-    }
-    return throwError("Something bad happened; please try again later.");
-  }
-
   delete(code: Customer,id:any): Observable<any> {
     console.log(`${this.baseApiUrl}/${id}`,code);
     return this.httpClient
