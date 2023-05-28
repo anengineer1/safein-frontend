222a3687ca24319f9d8d84b12c20798f35acef9a
Francisco Parra <franparpe@disroot.org>
2023-05-28 12:15:14 +0200

    Minor tweaks for making update handles, work

--- safein/src/app/services/customers.service.ts
+++ safein/src/app/services/customers.service.ts
@@ -1,65 +1,54 @@
 import { Injectable } from "@angular/core";
 import { BASE_API_URL } from "../global-config";
 import {
-  HttpClient,
-  HttpErrorResponse,
-  HttpHeaders,
+	HttpClient,
+	HttpErrorResponse,
+	HttpHeaders,
 } from "@angular/common/http";
 import { Observable, catchError, throwError } from "rxjs";
 import { Customer } from "../entityclasses/customer";
 
 @Injectable({
-  providedIn: "root",
+	providedIn: "root",
 })
 export class CustomersService {
-  baseApiUrl: string = BASE_API_URL + "customers"; // global bar located in app/config.ts
-  headers = new HttpHeaders().set("Content-Type", "application/json");
+	baseApiUrl: string = BASE_API_URL + "customers"; // global bar located in app/config.ts
+	headers = new HttpHeaders().set("Content-Type", "application/json");
 
-  constructor(private httpClient: HttpClient) {}
+	constructor(private httpClient: HttpClient) {}
 
-  listAllCustomers(): Observable<any> {
-    return this.httpClient
-      .get(this.baseApiUrl)
-      .pipe(catchError(this.handleError));
-  }
-  create(code: Customer): Observable<any> {
-    console.log(`${this.baseApiUrl}`, code);
-    return this.httpClient
-      .post(`${this.baseApiUrl}`, code)
-      .pipe(catchError(this.handleError));
-  }
+	listAllCustomers(): Observable<any> {
+		return this.httpClient
+			.get(this.baseApiUrl)
+			.pipe(catchError(this.handleError));
+	}
+	create(code: Customer): Observable<any> {
+		console.log(`${this.baseApiUrl}`, code);
+		return this.httpClient
+			.post(`${this.baseApiUrl}`, code)
+			.pipe(catchError(this.handleError));
+	}
 
-  getCustomerById(id: any): Observable<any> {
-    console.log(`${this.baseApiUrl}/${id}`);
-    return this.httpClient
-      .get(`${this.baseApiUrl}/${id}`)
-      .pipe(catchError(this.handleError));
-  }
+	getCustomerById(id: any): Observable<any> {
+		console.log(`${this.baseApiUrl}/${id}`);
+		return this.httpClient
+			.get(`${this.baseApiUrl}/${id}`)
+			.pipe(catchError(this.handleError));
+	}
 
-  handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      console.error("An error occurred:", error.error.message);
-    } else {
-      console.error(
-        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
-      );
-    }
-    return throwError("Something bad happened; please try again later.");
-  }
+	delete(code: Customer, id: any): Observable<any> {
+		console.log(`${this.baseApiUrl}/${id}`, code);
+		return this.httpClient
+			.delete(`${this.baseApiUrl}/${id}`)
+			.pipe(catchError(this.handleError));
+	}
 
-  delete(code: Customer,id:any): Observable<any> {
-    console.log(`${this.baseApiUrl}/${id}`,code);
-    return this.httpClient
-      .delete(`${this.baseApiUrl}/${id}`)
-      .pipe(catchError(this.handleError));
-  }
-  
-  update(code: Customer,id:any): Observable<any> {
-    console.log(`${this.baseApiUrl}/${id}`,code);
-    return this.httpClient
-      .put(`${this.baseApiUrl}/${id}`,code)
-      .pipe(catchError(this.handleError));
-  }
+	update(code: Customer, id: any): Observable<any> {
+		console.log(`${this.baseApiUrl}/${id}`, code);
+		return this.httpClient
+			.put(`${this.baseApiUrl}/${id}`, code)
+			.pipe(catchError(this.handleError));
+	}
 
 	getCustomerByMedia(media: any): Observable<any> {
 		console.log(`${this.baseApiUrl}/media/${media}`);
@@ -90,7 +79,6 @@ export class CustomersService {
 				`Backend returned code ${error.status}, ` +
 				`body was: ${error.error}`);
 		}
-		return throwError(
-			'Something bad happened; please try again later.');
+		return throwError(() => new Error('Something bad happened; please try again later.'));
 	};
 }
